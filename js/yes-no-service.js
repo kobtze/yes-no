8eaa859903b12bc631efb275e2f62462bf474b77
Kobi Donner <kobibo@gmail.com>
2020-06-15 23:48:58 +0300

    using jQuery $.get

--- js/yes-no-service.js
+++ js/yes-no-service.js
@@ -1,19 +1,11 @@
+'use strict';
+
+var ans = null;
 
 function getAns(onSuccess) {
-    var httpRequest = new XMLHttpRequest();
-
-    httpRequest.onreadystatechange =  () => {
-        // console.log('READINESS CHANGED:', httpRequest.readyState);
-        if (httpRequest.readyState === XMLHttpRequest.DONE &&
-             httpRequest.status === 200) {
-            const ans = JSON.parse(httpRequest.responseText)
-            onSuccess(ans);
-            // console.log('GOT:', httpRequest.responseText);
-            // console.log('GOT:', res);
-            // document.body.innerHTML = httpRequest.responseText;
-        }
-    }
-
-    httpRequest.open("GET", "https://yesno.wtf/api", true);
-    httpRequest.send();   
+    
+    $.get('https://yesno.wtf/api', (answer)=>{
+        ans = answer;
+        onSuccess(ans);
+    });
 }
